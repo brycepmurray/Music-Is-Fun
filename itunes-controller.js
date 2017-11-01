cff15a72c96326592a255e252703aafbdb9f7c05
Bryce Murray <brycepmurray711@gmail.com>
2017-11-01 16:26:17 -0600

    on to styling

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,18 +1,40 @@
-function ItunesController(){
-  var itunesService = new ItunesService()
-  //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
-    e.preventDefault();
-    var artist = e.target.artist.value;
-    itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
-  }
+function ItunesController() {
+    var itunesService = new ItunesService()
+        //Do Not Modify the getMusic function
+    this.getMusic = function getMusic(e) {
+        e.preventDefault();
+        var artist = e.target.artist.value;
+        itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
+    }
 
-  //Start coding here
-  function draw(){
-  }
+    //Start coding here
+    function draw(songList) {
+        var template = ''
+        for (var i = 0; i < songList.length; i++) {
+            var song = songList[i];
+            template += `
+    <div class="col-lg-6">
+            <h3>${song.title}</h3>
+             <img src="${song.albumArt}">
+           <h5>${song.artist}</h5>
+            <h5>${song.collection}</h4>
+            <h5>${song.price}</h4>
+            <audio controls class="audio">
+            <source src="${song.preview}" type="audio/ogg" >
+            </audio>
 
+    </div>
+`
+        }
+        document.getElementById("songs").innerHTML = template
+        document.addEventListener('play', function(e) {
+            var audios = document.getElementsByClassName('audio');
+            for (var i = 0, len = audios.length; i < len; i++) {
+                if (audios[i] != e.target) {
+                    audios[i].pause();
+                }
+            }
+        }, true);
 
-
-
-  
+    }
 }
